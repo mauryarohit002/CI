f1145f146d4f9aba0510d8942e9a318bf4357e07
mauryarohit002 <36223794+mauryarohit002@users.noreply.github.com>
2018-07-29 22:04:28 +0530

    01-07-2018

--- application/models/Crud.php
+++ application/models/Crud.php
@@ -1,14 +1,6 @@
 <?php
 	
-	class Crud extends CI_Model{
-
-		public function __construct()
-		{
-			parent::__construct();
-		}
-		function index(){
-			return $this->db->get('users');
-		}
+	class crud extends CI_Model{
 
 		function insert_data($data){
 
@@ -16,22 +8,6 @@
 
 			$this->db->insert("users",$data);
 		}
-		function deletedata($id)
-		{
-			$this->db->where("id",$id);
-			$this->db->delete("users");
-		}
-		// function editdata($id){
-		// 	$this->db->where("id",$id);
-		// 	$this->db->update("users");
-		// }
 
-		 function find_item($id)
-
-    	{
-
-        return $this->db->get_where('users', array('id' => $id))->row();
-
-    	}
 	}
 ?>
